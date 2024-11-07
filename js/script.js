99306f5b6b9af6ba5694bd2ffa9d5d9f7a1e6331
Pavlov-Vldmr <vpavlov95007@gmail.com>
2024-11-07 18:02:19 +0300

    countUp added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 $(document).ready(function () {
+  var options = {
+    useEasing: true,
+    useGrouping: true,
+    separator: ",",
+    decimal: ".",
+  };
+
+  // Find all Statistics on page, put them inside a variable
+  var statistics = $(".statistic");
+
+  // For each Statistic we find, animate it
+  statistics.each(function (index) {
+    // Find the value we want to animate (what lives inside the p tags)
+    var value = $(statistics[index]).html();
+    // Start animating
+    var statisticAnimation = new CountUp(
+      statistics[index],
+      0,
+      value,
+      0,
+      5,
+      options
+    );
+    statisticAnimation.start();
+  });
+
   $(".owl-carousel").owlCarousel({
     loop: true,
     items: 1,
@@ -16,3 +42,5 @@ $(document).ready(function () {
     owl.trigger("prev.owl.carousel", [300]);
   });
 });
+
+// These are the options that I'm going to be using on each statistic
